7d7363634551b4ac090b12b89f3d792c38a49640
BRIOTPatrick <51502604+BRIOTPatrick@users.noreply.github.com>
2019-09-07 03:37:31 +0200

    Update miniserial.h

--- miniserial.h
+++ miniserial.h
@@ -1,5 +1,5 @@
-#ifndef h//15436-1796
-#define h
+#ifndef MINISERIAL_h
+#define MINISERIAL_h
 
 // Version 1.00 31/08/2019 Final stable development -- footprint Code:328 RAM:18
 
@@ -9,43 +9,51 @@
 
 #define SERIALACTIVE                    // when not defined, functions below inactivated.
 
-class miniserial {
-public:
-  #ifdef SERIALACTIVE
-    #define byte(val,...)        data_((uint8_t)val, 0,##__VA_ARGS__)
-    #define word(val,...)        data_((uint16_t)val,1,##__VA_ARGS__)
-    #define long(val,...)        data_((uint32_t)val,2,##__VA_ARGS__)
-    #define print(ifsh)          print_(ifsh)
-    #define write(b)             write_(b)
-    #define read()               read_()
-    #define available()          available_()
-    #define begin(baud)          begin_(baud)
-  #else
-    #define byte(val,...)
-    #define word(val,...)
-    #define long(val,...)
-    #define print(ifsh)
-    #define write(b)
-    #define read()               (0)
-    #define available()          (0)
-    #define begin(baud)
-  #endif SERIALACTIVE
+#ifdef SERIALACTIVE
+#define MiniSerial_byte(val,...)        MiniSerial_data_((uint8_t)val, 0,##__VA_ARGS__)
+#define MiniSerial_word(val,...)        MiniSerial_data_((uint16_t)val,1,##__VA_ARGS__)
+#define MiniSerial_long(val,...)        MiniSerial_data_((uint32_t)val,2,##__VA_ARGS__)
+#define MiniSerial_print(ifsh)          MiniSerial_print_(ifsh)
+#define MiniSerial_write(b)             MiniSerial_write_(b)
+#define MiniSerial_read()               MiniSerial_read_()
+#define MiniSerial_available()          MiniSerial_available_()
+#define MiniSerial_begin(baud)          MiniSerial_begin_(baud)
+#else
+#define MiniSerial_byte(val,...)
+#define MiniSerial_word(val,...)
+#define MiniSerial_long(val,...)
+#define MiniSerial_print(ifsh)
+#define MiniSerial_write(b)
+#define MiniSerial_read()               (0)
+#define MiniSerial_available()          (0)
+#define MiniSerial_begin(baud)
+#endif SERIALACTIVE
 
-  #define FIFO_SIZE 16
+#define FIFO_SIZE 16
 
-private:
-  void    data_(uint32_t val, uint8_t bwl, uint8_t base=DEC, uint8_t zero=false);
-  void    print_(const __FlashStringHelper *ifsh);
-  void    write_(uint8_t b);
-  uint8_t read_();
-  uint8_t available_();
-  void    begin_(uint32_t baud);
+typedef struct {
+  uint8_t head;
+  uint8_t tail;
+  uint8_t buff[FIFO_SIZE];
+} FIFO;
 
-  typedef struct {
-    uint8_t head;
-    uint8_t tail;
-    uint8_t buff[FIFO_SIZE];
-  } FIFO;
-};
+// swap
+//uint8_t a = 10; uint8_t b = 20;
+//asm ( ""  : "=r" (a),"=r" (b)  : "0" (b),"1" (a));
 
-#endif  h
+#ifdef __cplusplus
+extern "C" {
+#endif __cplusplus
+
+void    MiniSerial_data_(uint32_t val, uint8_t bwl, uint8_t base=DEC, uint8_t zero=false);
+void    MiniSerial_print_(const __FlashStringHelper *ifsh);
+void    MiniSerial_write_(uint8_t b);
+uint8_t MiniSerial_read_();
+uint8_t MiniSerial_available_();
+void    MiniSerial_begin_(uint32_t baud);
+
+#ifdef __cplusplus
+}
+#endif __cplusplus
+
+#endif  MINISERIAL_h
